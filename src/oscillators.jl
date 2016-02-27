8a57336638aae2e55ff540aa78c1bd257374f1ae
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-02-27 10:47:41 +0200

    go reactive!

--- src/oscillators.jl
+++ src/oscillators.jl
@@ -1,4 +1,8 @@
+using Reactive
+
 export sine
 
-sine(ν, θ=0.0) =
-  (τ::Time, ι::AudioChannel) -> @fastmath 2.0muladd(ν, τ, θ) |> sinpi |> Sample
+sine(ν::Signal{Float64}, θ=Signal(Float64, 0)) =
+  (τ::Time, ι::AudioChannel) -> @fastmath 2.0muladd(value(ν), τ, value(θ)) |> sinpi |> Sample
+
+sine(ν, θ=0) = sine(Signal(Float64, ν), Signal(Float64, θ))

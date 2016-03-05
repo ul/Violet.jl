40a5573b796400325d15aef55fa5b8a0ec5f6651
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-03-05 10:04:09 +0200

    improve sine generator: * precompile returned function, no JIT hiccup on first play * used named function to improve performance (julia-0.4 specific optimisation, 0.5 solved this problem)

--- src/oscillators.jl
+++ src/oscillators.jl
@@ -1,6 +1,11 @@
 export sine
 
-sine(ν::Signal{Float64}, θ=Signal(Float64, 0)) =
-  (τ::Time, ι::AudioChannel) -> @fastmath 2.0muladd(value(ν), τ, value(θ)) |> sinpi |> Sample
+function sine(ν::Signal{Float64}, θ=Signal(Float64, 0))
+  function sine(τ::Time, ι::AudioChannel)
+    @fastmath 2.0muladd(value(ν), τ, value(θ)) |> sinpi |> Sample
+  end
+  precompile(sine, (Time, AudioChannel))
+  sine
+end
 
-sine(ν, θ=0) = sine(Signal(Float64, ν), Signal(Float64, θ))
+sine(ν, θ=0.0) = sine(Signal(Float64, ν), Signal(Float64, θ))

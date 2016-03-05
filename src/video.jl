b28dcedebacedbc7176bb86eda525d2450250554
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-03-05 09:23:02 +0200

    make oscillator width equal to the window width

--- src/video.jl
+++ src/video.jl
@@ -3,23 +3,24 @@ using Reactive, GLAbstraction, Colors, GeometryTypes, GLVisualize
 include("config.jl")
 include("circularbuffer.jl")
 
+maxscreenwidth = 4000
+
 function init(port=31337,
               buffer_size=CONFIG.hardware_buffer_size)
   global gport = port
   global window = glscreen()
-  global buffer = CircularBuffer(Float32, 20buffer_size)
+  global buffer = CircularBuffer(Float32, maxscreenwidth)
   global stream = convert(IO, connect(port))
   clock = fps(60)
   points = map(clock, window.inputs[:window_size]) do clock, size
     y₀ = size[2]/2
-    n = buffer.write_cursor - buffer_size
+    n = buffer.write_cursor - size[1]
     if n < 0
-      n += 20buffer_size
+      n += maxscreenwidth
     end
-    Point2f0[(i - 1, y₀*(1 + buffer[i + n])) for i=1:buffer_size]
+    Point2f0[(i - 1, y₀*(1 + buffer[i + n])) for i=1:size[1]]
   end
-  color = map(RGBA{Float32}, colormap("Blues", buffer_size))
-  view(visualize(points, :lines, color=color))
+  view(visualize(points, :lines))
 end
 
 function clean()

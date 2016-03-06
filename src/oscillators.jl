a18f1c6ac23019e818b66eef4b9c37aa0edac1a1
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-03-06 10:17:19 +0200

    make sine generator higher-order one (now FM-synth is trivial to do)

--- src/oscillators.jl
+++ src/oscillators.jl
@@ -1,11 +1,34 @@
 export sine
 
-function sine(ν::Signal{Float64}, θ=Signal(Float64, 0))
+function constantly(x)
+  s = Sample(x)
+  (τ::Time, ι::AudioChannel) -> s
+end
+
+signal(x::Signal{Float64}) =
+  (τ::Time, ι::AudioChannel) -> x |> value |> Sample
+
+signal(x::Signal{Array{Float64}}) =
+  (τ::Time, ι::AudioChannel) -> @inbounds x[ι] |> value |> Sample
+
+function sine(fν::Function, fθ::Function)
   function sine(τ::Time, ι::AudioChannel)
-    @fastmath 2.0muladd(value(ν), τ, value(θ)) |> sinpi |> Sample
+    ν = fν(τ, ι)
+    isnull(ν) && return Sample()
+    θ = fθ(τ, ι)
+    isnull(θ) && return Sample()
+    2.0muladd(get(ν), τ, get(θ)) |> sinpi |> Sample
   end
   precompile(sine, (Time, AudioChannel))
   sine
 end
 
-sine(ν, θ=0.0) = sine(Signal(Float64, ν), Signal(Float64, θ))
+function sine(ν::Signal{Float64}, θ=Signal(Float64, 0.0))
+  sine(signal(ν), signal(θ))
+end
+
+function sine(ν::Signal{Float64}, θ::Function)
+  sine(signal(ν), θ)
+end
+
+sine(ν, θ=0.0) = sine(constantly(ν), constantly(θ))
